8e795743fd9a92d186543bf342bf94b9f748ffbc
Eduard Tomasek <tomasek@organizetube.com>
2020-07-17 10:28:15 +0200

    lesson 6

---
+++ src/views/NetworkIssue.vue
@@ -0,0 +1,7 @@
+<template>
+	<div>
+		<h1>Oh-Oh!</h1>
+		<h3>It looks like you're experiencing some network issues.</h3>
+		<router-link :to="{ name: 'event-list' }">Go back to the home page</router-link>
+	</div>
+</template>
