f1f85213332fbf136a6e00bdb45132c48bb3d6bf
Eduard Tomasek <tomasek@organizetube.com>
2020-07-17 13:19:32 +0200

    lesson 10

--- src/router.js
+++ src/router.js
@@ -7,6 +7,7 @@ import NProgress from 'nprogress'
 import store from '@/store/store'
 import NotFound from './views/NotFound.vue'
 import NetworkIssue from './views/NetworkIssue.vue'
+import Example from './views/Example.vue'
 
 Vue.use(Router)
 
@@ -44,6 +45,10 @@ const router = new Router({
             }
           })
       }
+    },
+    {
+      path: '/example',
+      component: Example
     },
     {
       path: '/404',
